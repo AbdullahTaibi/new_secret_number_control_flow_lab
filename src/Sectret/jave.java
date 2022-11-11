95abe7179acec8fb14f3cbb2461eaf655272a93f
Abdullah <abdullah123-@hotmail.co.uk>
2022-11-11 13:53:43 +0000

    added secret number

---
+++ src/Sectret/jave.java
@@ -0,0 +1,29 @@
+// DataTypes.java
+import javax.swing.*;
+import java.util.Random;
+import java.util.Scanner;
+
+public class SecretNumber {
+
+    public static void main(String[] args){
+
+        Random rand = new Random();
+
+        int randomNumber = rand.nextInt(50) + 1;
+
+        System.out.println("Please enter your answer (1-100):");
+
+        Scanner scanner = new Scanner(System.in);
+                int playerGuess = scanner.nextInt();
+
+        if (playerGuess == randomNumber){
+            System.out.println("Well Done!");
+        } else if (randomNumber > playerGuess) {
+            System.out.println("Try Again");
+
+
+            }
+
+        }
+    }
+
